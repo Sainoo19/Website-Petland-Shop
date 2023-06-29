aabbce145d49cb07a62cd98e36b384dac10faf99
Sainoo <122950791+Sainoo19@users.noreply.github.com>
2023-06-29 23:29:57 +0700

    Update User
    Home + About + Contact

--- Petland Shop/Controllers/HomeController.cs
+++ Petland Shop/Controllers/HomeController.cs
@@ -18,6 +18,15 @@ namespace Petland_Shop.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
